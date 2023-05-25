5fd6282ee23fcca08bbb29f214ded73540899145
kimtaeyoon20031220 <k0278kim@gmail.com>
2023-05-25 16:04:21 +0900

    skill 이미지, 이름 넣기, 더미데이터를 실제 데이터로 변경

--- skill.js
+++ skill.js
@@ -1,72 +1,87 @@
 dev_data = [
     {
-        "image" : "https://raw.githubusercontent.com/github/explore/f3e22f0dca2be955676bc70d6214b95b13354ee8/topics/c/c.png",
-        "percent" : "70%",
+        "image" : "https://i.namu.wiki/i/KcqDuQYTxNpUcLIMZTg28QXse0XiWx1G7K68kYYCo1GuhoHmhB_V8Qe9odGGt0BH9-0nQZTN53WXTNpDmwVfWQ.svg",
+        "ability" : "medium",
         "name" : "C"
     },
     {
-        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
-        "percent" : "40%",
+        "image" : "https://i.namu.wiki/i/k2nwDQNWZhSfRdbz2nPQzERjwjuWx3xxFHm3Q2m5xf868NeTGOQaGHOw_MhcqlkHqpCgk1GsLgCgUp6X-gnReiuWgGAIZjmHeHFRtWhkazrrEbfLc8fAEy6B2EpGDG9rSn1Q1nssQ7eUXB2aDSbJGg.svg",
+        "ability" : "medium",
         "name" : "C++"
     },
     {
+        "image" : "https://i.pinimg.com/originals/6d/b1/59/6db159df526b6f5584902ebc21daca88.png",
+        "ability" : "medium",
+        "name" : "Flutter"
+    },
+    {
         "image" : "https://blog.kakaocdn.net/dn/bpzJ8L/btqAHJxe1Ib/x0ZAG10ADHAtc9ItRkLqp0/img.png",
-        "percent" : "50%",
-        "name" : ""
-    }
+        "ability" : "medium",
+        "name" : "Kotlin"
+    },
+    {
+        "image" : "https://3.bp.blogspot.com/-VVp3WvJvl84/X0Vu6EjYqDI/AAAAAAAAPjU/ZOMKiUlgfg8ok8DY8Hc-ocOvGdB0z86AgCLcBGAsYHQ/s1600/jetpack%2Bcompose%2Bicon_RGB.png",
+        "ability" : "medium",
+        "name" : "Jetpack Compose"
+    },
+    {
+        "image" : "https://blog.kakaocdn.net/dn/9ZCsE/btqFp43DyIM/c7KHGrjY8OI3bYZvyQeej0/img.png",
+        "ability" : "high",
+        "name" : "HTML/CSS"
+    },
+    {
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        "ability" : "medium",
+        "name" : "Javascript"
+    },
+    {
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+        "ability" : "high",
+        "name" : "Python"
+    },
+    {
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/1915px-Tensorflow_logo.svg.png",
+        "ability" : "low",
+        "name" : "tensorflow"
+    },
 ]
 
 plan_data = [
     {
-        "image" : "https://raw.githubusercontent.com/github/explore/f3e22f0dca2be955676bc70d6214b95b13354ee8/topics/c/c.png",
-        "percent" : "70%",
-        "name" : "C"
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1667px-Figma-logo.svg.png",
+        "ability" : "medium",
+        "name" : "Figjam"
     },
     {
-        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
-        "percent" : "40%",
-        "name" : "C++"
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Notion-logo.svg/1024px-Notion-logo.svg.png",
+        "ability" : "medium",
+        "name" : "Notion"
     },
-    {
-        "image" : "https://blog.kakaocdn.net/dn/bpzJ8L/btqAHJxe1Ib/x0ZAG10ADHAtc9ItRkLqp0/img.png",
-        "percent" : "50%",
-        "name" : ""
-    }
 ]
 
 design_data = [
     {
-        "image" : "https://raw.githubusercontent.com/github/explore/f3e22f0dca2be955676bc70d6214b95b13354ee8/topics/c/c.png",
-        "percent" : "70%",
-        "name" : "C"
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1667px-Figma-logo.svg.png",
+        "ability" : "high",
+        "name" : "Figma"
     },
-    {
-        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
-        "percent" : "40%",
-        "name" : "C++"
-    },
-    {
-        "image" : "https://blog.kakaocdn.net/dn/bpzJ8L/btqAHJxe1Ib/x0ZAG10ADHAtc9ItRkLqp0/img.png",
-        "percent" : "50%",
-        "name" : ""
-    }
 ]
 
 com_data = [
     {
-        "image" : "https://raw.githubusercontent.com/github/explore/f3e22f0dca2be955676bc70d6214b95b13354ee8/topics/c/c.png",
-        "percent" : "70%",
-        "name" : "C"
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg",
+        "ability" : "high",
+        "name" : "KakaoTalk"
     },
     {
-        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
-        "percent" : "40%",
-        "name" : "C++"
+        "image" : "https://user-images.githubusercontent.com/23306911/71767498-1d3f7600-2f0d-11ea-9f8e-c0e2e8be8dff.png",
+        "ability" : "high",
+        "name" : "Excalidraw"
     },
     {
-        "image" : "https://blog.kakaocdn.net/dn/bpzJ8L/btqAHJxe1Ib/x0ZAG10ADHAtc9ItRkLqp0/img.png",
-        "percent" : "50%",
-        "name" : ""
+        "image" : "https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png",
+        "ability" : "high",
+        "name" : "Discord"
     }
 ]
 
@@ -76,50 +91,40 @@ plan = document.querySelector("#plan");
 design = document.querySelector("#design");
 com = document.querySelector("#com");
 
-dev.innerHTML += "<span class='skill-title'>Develop</span>"
-for(let i = 0; i < dev_data.length; i++) {
-    dev.innerHTML += 
-    "<div class='skill-item'>" +
-        "<div class='skill-icon' style='background-image: url('" + dev_data[i]["image"] + "')'></div>" +
-        "<div class='skill-bar'>" +
-            "<div class='skill-bar-back'></div>" +
-            "<div class='skill-bar-front' style='width: " + dev_data[i]["percent"] + "; background-color: orange;'></div>" +
-        "</div>" +
-    "</div>"    
-}
+dataBind(dev, dev_data, "Develop");
+dataBind(plan, plan_data, "Plan");
+dataBind(design, design_data, "Design");
+dataBind(com, com_data, "Communicate");
 
-plan.innerHTML += "<span class='skill-title'>Plan</span>"
-for(let i = 0; i < plan_data.length; i++) {
-    plan.innerHTML += 
-    "<div class='skill-item'>" +
-        "<div class='skill-icon' style='background-image: url('" + plan_data[i]["image"] + "')'></div>" +
-        "<div class='skill-bar'>" +
-            "<div class='skill-bar-back'></div>" +
-            "<div class='skill-bar-front' style='width: " + plan_data[i]["percent"] + "; background-color: orange;'></div>" +
-        "</div>" +
-    "</div>"    
-}
+function dataBind(obj, data, title) {
+    obj.innerHTML += "<span class='skill-title'>" + title + "</span>"
+    for(let i = 0; i < data.length; i++) {
+        let color = "#4387ED";
+        let width = "100%";
+        if (data[i]["ability"] == "high") {
+            color = "#4387ED";
+            width = "100%";
+        } else if (data[i]["ability"] == "medium") {
+            color = "orange";
+            width = "66%";
+        } else {
+            color = "#FC5960";
+            width = "33%";
+        }
+        obj.innerHTML += 
+        "<div class='skill-item'>" +
+            `<div class='skill-icon'></div>` +
+            "<div class='skill-content'>" + 
+                `<div class='skill-name'>${data[i]["name"]}</div>`+ 
+                "<div class='skill-bar'>" +
+                    "<div class='skill-bar-back'></div>" +
+                    "<div class='skill-bar-front' style='width: " + width + "; background-color: "+ color +";'></div>" +
+                "</div>" +
+            "</div>"
+        "</div>"    
 
-design.innerHTML += "<span class='skill-title'>Design</span>"
-for(let i = 0; i < design_data.length; i++) {
-    design.innerHTML += 
-    "<div class='skill-item'>" +
-        "<div class='skill-icon' style='background-image: url('" + design_data[i]["image"] + "')'></div>" +
-        "<div class='skill-bar'>" +
-            "<div class='skill-bar-back'></div>" +
-            "<div class='skill-bar-front' style='width: " + design_data[i]["percent"] + "; background-color: orange;'></div>" +
-        "</div>" +
-    "</div>"    
-}
+        obj.querySelectorAll(".skill-icon")[i].style.backgroundImage = `url("${data[i]["image"]}")`
 
-com.innerHTML += "<span class='skill-title'>Communicate</span>"
-for(let i = 0; i < com_data.length; i++) {
-    com.innerHTML += 
-    "<div class='skill-item'>" +
-        "<div class='skill-icon' style='background-image: url('" + com_data[i]["image"] + "')'></div>" +
-        "<div class='skill-bar'>" +
-            "<div class='skill-bar-back'></div>" +
-            "<div class='skill-bar-front' style='width: " + com_data[i]["percent"] + "; background-color: orange;'></div>" +
-        "</div>" +
-    "</div>"    
+        console.log(data[i]["image"])
+    }
 }
